f573613308da142eca56da0b30ebac6c6c8788fa
Bernardroid <bernardogaribaybravo@gmail.com>
2019-05-15 03:51:29 -0500

    4 am commits are the best

---
+++ Assets/Scripts/CandyCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyCollision : MonoBehaviour {
+
+    public GameObject starsPrefab;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag("Alebrije"))
+        {
+            Instantiate(starsPrefab, collision.transform);
+            Destroy(this.gameObject);
+        }
+    }
+}
